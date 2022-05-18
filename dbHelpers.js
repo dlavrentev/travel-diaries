9a10e272a910d2763cae489201e741cf543c81d0
dlavrentev <dimitrilavrentev96@gmail.com>
2022-05-18 19:32:55 +0200

    backend session 5

--- dbHelpers.js
+++ dbHelpers.js
@@ -4,7 +4,7 @@ const db = knex(config.development)
 
 
 function getAllUsers(){
-    return db('users')
+    return db('users').orderBy('id','desc')
 }
 
 async function addUser(user){
@@ -21,10 +21,40 @@ function removeUser(id){
     return db('users').where({id:id}).del()
 }
 
+
+function getAllDestinations(){
+    return db('destinations').orderBy('id','desc')
+}
+
+function findUserById(id){
+    return db('users').where({id:id}).first()
+}
+
+async function addDestination(){
+    await db('destinations')
+    .where({user_id:newDestination.user_id})
+    .insert(newDestination)
+}
+
+function removeDestination(id){
+    return db('destinations').where({id:id}).del()
+}
+
+function updateDestination(id,newDestination){
+     return db('destination')
+     .where({id:id})
+     .update(newDestination)
+}
+
 module.exports = {
     getAllUsers,
     addUser,
     findUserByUsername,
-    removeUser
+    removeUser,
+    getAllDestinations,
+    findUserById,
+    addDestination,
+    removeDestination,
+    updateDestination
 }
 
--- index.js
+++ index.js
@@ -99,10 +99,75 @@ app.post('/users/login', (req,res)=>{
         if(user && bcrypt.compareSync(password,user.password)){
             res.status(200).json(user)
         } else {
-            res.status(400).json({message:"User with that password does not exist."})
+            res.status(404).json({message:"User with that password does not exist."})
         }
     })
 })
+
+// DESTINATIONS
+
+app.get('/destinations',(req,res)=>{
+    Travels.getAllDestinations()
+    .then(destinations=>{
+        res.status(200).json(destinations)
+    })
+    .catch(error=>res.status(500).json(error))
+})
+
+
+app.post('/users/:id/destinations',(req,res)=>{
+    const {id}= req.params;
+    const newDestination = req.body
+    newDestination["user_id"] = id;
+
+    Travels.findUserById(id)
+    .then(user=>{
+        if(!user){
+            res.status(404).json({message:"User does not exist."})
+        } else {
+            if(!newDestination.title || !newDestination.description){
+                res.status(400).json({message:"All fields must be complete"})
+            } else {
+                Travels.addDestination(newDestination)
+                .then(destination=>{
+                    res.status(200).json(destination)
+                })
+                .catch(error=>res.status(500).json(error))
+            }
+        }
+    })
+    .catch(error=>res.status(500).json(error))
+
+})
+
+
+app.delete('/destinations/:id',(req, res)=>{
+    
+    // const id = req.params.id
+    const {id} = req.params
+    
+    Travels.removeDestination(id)
+    .then(count=>{
+        if(count>0){
+            res.status(200).json({message:"Destination is deleted"})
+        }else{
+            res.status(404).json({message:"No destination with that id"})
+        }
+    })
+    .catch(error=>res.status(500).json(error))
+})
+
+
+app.patch('destinations/:id',(req,res)=>{
+    const {id} = req.params;
+    Travels.updateDestination(id,req.body)
+    .then(destination=>{
+        res.status(200).json({message:"Destination updated"})
+    })
+    .catch()
+})
+
+
 
 
 
