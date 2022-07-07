a10a780438145fe777686602bfadac0ac0378c53
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-07 10:39:27 +0200

    Update Details.js

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -3,8 +3,20 @@ import './../Details/details.css'
 
 const Details = () => {
   return (
-    <div className='details-container'>
-
+    <div className='details__container'>
+         <div className='details__top'>
+            <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'></img>
+            <img></img>
+            <h2></h2>
+         </div>
+         <div className='details__mid'>
+            <h1></h1>
+            <p></p>
+         </div>
+         <div className='details__bot'>
+            <h2></h2>
+            <h2></h2>
+         </div>
     </div>
   )
 }
