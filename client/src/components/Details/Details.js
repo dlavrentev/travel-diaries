6136aa7b22cfff7a7bb1f14d321495a2d5c7d734
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-07 10:47:51 +0200

    updated details component

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -5,17 +5,18 @@ const Details = () => {
   return (
     <div className='details__container'>
          <div className='details__top'>
-            <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'></img>
-            <img></img>
-            <h2></h2>
+            <img className="details__profileimg" src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'></img>
+            <img className="details__cityimg" src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'></img>
+            <h2>15</h2>
          </div>
          <div className='details__mid'>
-            <h1></h1>
-            <p></p>
+            <h1>Paris</h1>
+            <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
          </div>
          <div className='details__bot'>
-            <h2></h2>
-            <h2></h2>
+            <h2>username123515</h2>
+            <h2>created: 07.07.2022</h2>
          </div>
     </div>
   )
