206e55c08cb48a6e70678fdae369813bae695cf2
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-08 11:43:39 +0200

    Update Details.js

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import './../Details/details.css'
 
-const Details = ({destination}) => {
+const Details = ({destinations}) => {
   return (
     <div className='details__body'>
         <div className='details__container'>
          <div className='details__top'>
-            <div className="details__profileimg"><img src='https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg'></img></div>
-            <div className="details__cityimg"><img src={destination.imageUrl}></img></div>
-            <h2 className='details__h2'>15</h2>
+            <div className="details__profileimg"><img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'></img></div>
+            <div className="details__cityimg"><img src={destinations.imageUrl}></img></div>
+            <h2 className='details__h2'>{destinations.user_id}</h2>
          </div>
          <div className='details__mid'>
-            <h1>{destination.title}</h1>
-            <p>{destination.description}</p>
+            <h1>{destinations.title}</h1>
+            <p>{destinations.description}</p>
          </div>
          <div className='details__bot'>
-            <h2>username123515</h2>
-            <h2>created: 07.07.2022</h2>
+            <h2>username</h2>
+            <h2>created</h2>
          </div>
     </div>
     </div>
