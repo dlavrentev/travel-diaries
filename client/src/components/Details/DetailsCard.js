a71bc76f3e380423a5450818ce3c6c40c2d4124c
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-08 11:57:52 +0200

    Update DetailsCard.js

--- client/src/components/Details/DetailsCard.js
+++ client/src/components/Details/DetailsCard.js
@@ -3,17 +3,17 @@ import './../Details/details.css'
 
 const DetailsCard = ({destination}) => {
 
-  console.log(destination.user_id)
+
   return (
         <div className='details__container'>
          <div className='details__top'>
             <div className="details__profileimg"><img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'></img></div>
-            <div className="details__cityimg"><img src={destination.imageUrl}></img></div>
-            <h2 className='details__h2'>{destination.user_id}</h2>
+            <div className="details__cityimg"><img src={destination[0].imageUrl}></img></div>
+            <h2 className='details__h2'>{destination[0].user_id}</h2>
          </div>
          <div className='details__mid'>
-            <h1>{destination.title}</h1>
-            <p>{destination.description}</p>
+            <h1>{destination[0].title}</h1>
+            <p>{destination[0].description}</p>
          </div>
          <div className='details__bot'>
             <h2>username</h2>
