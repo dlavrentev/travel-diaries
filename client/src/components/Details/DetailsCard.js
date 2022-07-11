33b0b59be6a3a6d4c4bd89278453701fe162ddb1
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-11 11:26:44 +0200

    Update DetailsCard.js

--- client/src/components/Details/DetailsCard.js
+++ client/src/components/Details/DetailsCard.js
@@ -8,14 +8,15 @@ import { useParams } from 'react-router-dom'
 const DetailsCard = ({destinations, usercontext}) => {
 
    
-
+const [user,setUser] = useState([]);
+const [currentDestination, setCurrentDestination] = useState([]);
 
 
 const {cityid} = useParams();
 
-const index = destinations.findIndex( (element) => element.id == cityid);
-const userId = destinations[index].user_id;
-const userIndex = destinations.findIndex( (element) => element.user_id == userId);
+// const index = destinations.findIndex( (element) => element.id == cityid);
+// const userId = destinations[index].user_id;
+// const userIndex = destinations.findIndex( (element) => element.user_id == userId);
 
 
 useEffect(() => {
@@ -36,12 +37,12 @@ useEffect(() => {
         <div className='details__container'>
          <div className='details__top'>
             <div className="details__profileimg"><img alt='profile'src={usercontext[userIndex].imageUrl}></img></div>
-            <div className="details__cityimg"><img alt='city' src={destinations[index].imageUrl}></img></div>
-            <h2 className='details__h2'>{destinations[index].id}</h2>
+            <div className="details__cityimg"><img alt='city' src={currentDestination.imageUrl}></img></div>
+            <h2 className='details__h2'>{currentDestination.id}</h2>
          </div>
          <div className='details__mid'>
-            <h1>{destinations[index].title}</h1>
-            <p>{destinations[index].description}</p>
+            <h1>{currentDestination.title}</h1>
+            <p>{currentDestination.description}</p>
          </div>
          <div className='details__bot'>
             <h2>{usercontext[userIndex].username}</h2>
